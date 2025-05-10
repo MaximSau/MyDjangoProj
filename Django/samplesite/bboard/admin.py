baa94827f6b5c9bcbc36cc7501b6111828be8398
MaximSau <clashminiclashmini016@gmail.com>
2025-05-10 14:42:48 +0300

    Update admin.py

--- Django/samplesite/bboard/admin.py
+++ Django/samplesite/bboard/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Bd, Rubric
+from .models import Bd, Rubric, Novelty
 
 class BdAdmin(admin.ModelAdmin):
     list_display = ('rubric', 'title', 'content', 'price', 'published')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
-admin.site.register(Rubric)
+admin.site.register(Novelty)
